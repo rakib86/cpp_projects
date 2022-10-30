4bbeee45a1773f5b78ede02f5164a13b60f8b58f
rakiburrahamanCS <mail.rakiburrahaman@gmail.com>
2022-10-30 21:29:23 +0600

    upload sqr.cpp

---
+++ squre.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+
+    // while loop
+
+    //or 
+    while ( i < 10 ){
+        int sqr = i * i;
+        cout<<i<<"   "<<sqr<<"\n";
+        i++;
+    }
+
+}
